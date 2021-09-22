2f7516e3e072ec71f616f754403fab78883fcc30
Aelina29 <58300777+Aelina29@users.noreply.github.com>
2021-09-22 21:44:23 +0300

    первая версия
    просто отображение без редакции, без выбора исходника. Трабла - r в результате отрицательный. Причина предполагаемая: округление int double

--- lab2/Form2.Designer.cs
+++ lab2/Form2.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(523, 415);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(635, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hue(Тон)";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(555, 71);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(233, 56);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(555, 189);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(233, 56);
+            this.trackBar2.TabIndex = 5;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(555, 301);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(233, 56);
+            this.trackBar3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(551, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Saturation(Насыщенность)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(578, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Brightness(Яркость)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(569, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 55);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Преобразовать и сохранить в файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- lab2/Form2.cs
+++ lab2/Form2.cs
@@ -10,11 +10,170 @@ using System.Windows.Forms;
 
 namespace lab2
 {
+    struct RGB
+    {
+        public double r;
+        public double g;
+        public double b;
+        public RGB(double r, double g, double b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+    struct HSV
+    {
+        public double h;
+        public double s;
+        public double v;
+        public HSV(double h, double s, double v)
+        {
+            this.h = h;
+            this.s = s;
+            this.v = v;
+        }
+    }
     public partial class Form2 : Form
     {
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("../../PicForTask3.jpg");
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            for (int y = 0; y < bmp.Height; y++)
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    RGB original_rgb = new RGB (c.R, c.G, c.B);
+                    HSV original_hsv = RGB_HSV(original_rgb, x, y);
+                    RGB modifided_rgb = HSV_RGB(original_hsv);
+                    //if (x == 92 && y == 182)
+                    //{
+                    //    original_rgb.r *= 1;
+                    //    original_rgb.g *= 1;
+                    //    original_rgb.b *= 1;
+                    //    original_hsv.h *= 1;
+                    //    original_hsv.s *= 1;
+                    //    original_hsv.v *= 1;
+                    //    modifided_rgb.r *= 1;
+                    //    modifided_rgb.g *= 1;
+                    //    modifided_rgb.b *= 1;
+                    //}
+                    bmp.SetPixel(x, y, Color.FromArgb(255, (int)modifided_rgb.r , (int)modifided_rgb.g, (int)modifided_rgb.b));
+                }
+            pictureBox1.Image = (Bitmap)bmp;
+        }
+        //преобразование RGB->HSV
+        HSV RGB_HSV(RGB rgb, int x, int y)
+        {
+            HSV hsv = new HSV(0, 0, 0);
+            rgb.r /= 255;
+            rgb.g /= 255;
+            rgb.b /= 255;
+            int max = Math.Max((int)rgb.r, Math.Max((int)rgb.g, (int)rgb.b));
+            int min = Math.Min((int)rgb.r, Math.Min((int)rgb.g, (int)rgb.b));
+            bool f = false;
+            if (max == min) hsv.h = 0;
+            else 
+            {
+                if(max == rgb.r)
+                {
+                    hsv.h = 60 * ((rgb.g - rgb.b) / (max - min));
+                    if (rgb.g < rgb.b)
+                        hsv.h += 360;
+                }
+                else
+                {
+                    if (max == rgb.g)
+                    {
+                        hsv.h = 60 * ((rgb.b - rgb.r) / (max - min)) + 120;
+                        //if (x == 92 && y == 182) 
+                            //f = true;
+                    }
+                    else
+                    {
+                        if (max == rgb.b)
+                            hsv.h = 60 * ((rgb.r - rgb.g) / (max - min)) + 240;
+                    }
+                }
+            }
+            if (max == 0) hsv.s = 0;
+            else hsv.s = 1 - (min / max);
+            hsv.v = max;
+            hsv.s *= 100;
+            hsv.v *= 100;
+            return hsv;
+        }
+        //преобразование HSV->RGB
+        RGB HSV_RGB(HSV hsv)
+        {
+            RGB rgb = new RGB(0,0,0);
+            double vmin = ((100 - hsv.s) * hsv.v) / 100;
+            double a = (hsv.v - vmin) * (hsv.h % 60) / 60;
+            double vinc = vmin + a;
+            double vdec = vmin - a;
+            int hi = (int)Math.Floor((hsv.h / 60)) % 6;
+            switch(hi)
+            {
+                case 0:
+                    rgb.r = hsv.v;
+                    rgb.g = vinc;
+                    rgb.b = vmin;
+                    break;
+                case 1:
+                    rgb.r = vdec;
+                    rgb.g = hsv.v;
+                    rgb.b = vmin;
+                    break;
+                case 2:
+                    rgb.r = vmin;
+                    rgb.g = hsv.v;
+                    rgb.b = vinc;
+                    break;
+                case 3:
+                    rgb.r = vmin;
+                    rgb.g = vdec;
+                    rgb.b = hsv.v;
+                    break;
+                case 4:
+                    rgb.r = vinc;
+                    rgb.g = vmin;
+                    rgb.b = hsv.v;
+                    break;
+                case 5:
+                    rgb.r = hsv.v;
+                    rgb.g = vmin;
+                    rgb.b = vdec;
+                    break;
+                default:
+                    break;
+            }
+            rgb.r *= 2.55;
+            rgb.g *= 2.55;
+            rgb.b *= 2.55;
+            return rgb;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("../../PicForTask3.jpg");
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            for (int y = 100; y < bmp.Height; y++)
+                for (int x = 100; x < bmp.Width; x++)
+                {
+                    //Color c = bmp.GetPixel(x, y);
+                    //RGB original_rgb = new RGB(c.R, c.G, c.B);
+                    //HSV original_hsv = RGB_HSV(original_rgb);
+                    //HSV modifided_hsv = взять из ползунков
+                    //RGB modifided_rgb = HSV_RGB(modifided_hsv);
+                    //bmp.SetPixel(x, y, Color.FromArgb(255, modifided_rgb.r, modifided_rgb.g, modifided_rgb.b));
+                }
+            pictureBox1.Image = (Bitmap)bmp;
         }
     }
 }
